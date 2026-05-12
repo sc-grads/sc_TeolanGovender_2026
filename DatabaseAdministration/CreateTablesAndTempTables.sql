824f54ce20134530f2c44d947b076050ac1003ef
Teolan432 <teolan.govender@sambeconsulting.com>
2026-05-12 09:14:30 +0200

    SQL course - creating tables and temp tables

---
+++ DatabaseAdministration/CreateTablesAndTempTables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE [AdventureWorks2019].[sales].[visits] (
+visit_id INT PRIMARY KEY IDENTITY (1, 1),
+first_name VARCHAR (50) NOT NULL,
+last_naame VARCHAR (50) NOT NULL,
+visited_at DATETIME,
+phone VARCHAR(20),
+store_id INT NOT NULL,
+FOREIGN KEY (store_id) REFERENCES sales.storenew(store_id)
+)
+
+CREATE TABLE [AdventureWorks2019].[sales].[storenew] (
+store_id INT NOT NULL,
+sales INT
+)
+
+SELECT * FROM [AdventureWorks2019].[sales].[storenew]
+SELECT * FROM [AdventureWorks2019].[sales].[visits]
+
+select BusinessEntityID, firstname,lastname, Title
+into #TempPersonTable
+from [Person].[Person]
+where title = 'mr.'
+
+
+SELECT * FROM #TempPersonTable
