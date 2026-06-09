6a24f2876d288e8002db14a4abefebbab04e0f36
Teolan432 <teolan.govender@sambeconsulting.com>
2026-06-09 15:35:28 +0200

    Update CreateTable.sql

--- SQL-Project/scripts/CreateTable.sql
+++ SQL-Project/scripts/CreateTable.sql
@@ -1,7 +1,8 @@
 IF OBJECT_ID('Users') IS NULL
 BEGIN
     CREATE TABLE Users (
-        Id INT PRIMARY KEY IDENTITY,
-        Name NVARCHAR(100)
+        user_id INT IDENTITY(1,1) PRIMARY KEY,
+        first_name NVARCHAR(100) NOT NULL,
+        last_name NVARCHAR(100) NOT NULL
     );
 END
