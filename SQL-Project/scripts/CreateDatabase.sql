c5acf5ca9d5a7ae2305e6c6c26dc6f64308ca6b0
Teolan432 <teolan.govender@sambeconsulting.com>
2026-06-09 15:06:20 +0200

    deployment-workflow update4

--- SQL-Project/scripts/CreateDatabase.sql
+++ SQL-Project/scripts/CreateDatabase.sql
@@ -4,3 +4,7 @@ BEGIN
     CREATE DATABASE Cloud_Tunnels_TG;
 END
 GO
+
+USE [Cloud_Tunnels_TG];
+CREATE USER automation_user FOR LOGIN automation_user;
+ALTER ROLE db_owner ADD MEMBER automation_user;
--- SQL-Project/scripts/Permissions.sql
+++ SQL-Project/scripts/Permissions.sql
@@ -1,5 +1,5 @@
 --Permissions.sql
 
-USE [Cloud-Tunnels-TG];
+USE [Cloud_Tunnels_TG];
 CREATE USER automation_user FOR LOGIN automation_user;
 ALTER ROLE db_owner ADD MEMBER automation_user;
