308edc53552e0d6a3f22ea9c7c1127e77ecc09b4
Teolan432 <teolan.govender@sambeconsulting.com>
2026-06-10 09:56:30 +0200

    update-directory

--- Mastering-SSIS/Resources/SSISDemoFiles/ssisdemofiles/PostalCodes.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE PRODSSIS_ZIPCODE 
-(
-ZipCode Char(5),
-State Char(2),
-ZipName Varchar(16)
-)
--- SQL-Project/scripts/Permissions.sql
+++ SQL-Project/scripts/Permissions.sql
@@ -1,17 +1,5 @@
 --Permissions.sql
-USE Cloud_Tunnels_TG;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM sys.database_principals
-    WHERE name = 'automation_user'
-)
-BEGIN
-    CREATE USER automation_user
-    FOR LOGIN automation_user;
-END
-GO
-
+--only works on DB owners server
 ALTER ROLE db_owner
 ADD MEMBER automation_user;
 GO
