c4d4147e464683743dca47fd7d34242e6906f3e5
Teolan432 <teolan.govender@sambeconsulting.com>
2026-07-09 08:44:56 +0200

    WorkflowUpdate102

--- SSIS-Project/Scripts/ProvisionJob.sql
+++ SSIS-Project/Scripts/ProvisionJob.sql
@@ -1,6 +1,5 @@
 USE [msdb];
 GO
-
 SET NOCOUNT ON;
 
 -- 1. Clear Out Stale Existing Job Definitions to Avoid Metadata Collisions
@@ -12,24 +11,56 @@ GO
 
 -- 2. Provision the Core Automated Job Agent (No Email Alerts)
 DECLARE @jobId BINARY(16);
-
-EXEC msdb.dbo.sp_add_job 
+EXEC msdb.dbo.sp_add_job     
     @job_name = N'Run_Timesheet_Package', 
     @enabled = 1, 
     @job_id = @jobId OUTPUT;
 
--- 3. Add Execution Step Target explicitly pointing at Package2.dtsx (Cleaned Syntax)
-DECLARE @ssisCommand NVARCHAR(4000) = N'/ISSERVER "\"\SSISDB\PracticeActivities\TimesheetIntegrationTestTwo\Package2.dtsx\"" /SERVER "."';
+-- 3. Add Execution Step Target running as T-SQL to utilize the Database Engine File Access
+-- This executes the catalog package directly and applies the correct parameter path mapping
+DECLARE @tsqlCommand NVARCHAR(MAX) = N'
+    DECLARE @execution_id BIGINT;
+    
+    -- Create the execution instance in the SSIS catalog
+    EXEC [SSISDB].[catalog].[create_execution] 
+        @folder_name = N''PracticeActivities'', 
+        @project_name = N''TimesheetIntegrationTestTwo'', 
+        @package_name = N''Package2.dtsx'', 
+        @reference_id = NULL, 
+        @use32bitruntime = FALSE, 
+        @execution_id = @execution_id OUTPUT;
+    
+    -- Try setting it via Project Scope (Object Type 20)
+    BEGIN TRY
+        EXEC [SSISDB].[catalog].[set_execution_parameter_value] 
+            @execution_id, 
+            @object_type = 20, 
+            @parameter_name = N''Source_File_Directory'', 
+            @parameter_value = N''c:\sc_CharmaneMchunu_2026\SSIS-Project-TG\Timesheets\'';
+    END TRY
+    BEGIN CATCH
+        -- Fallback to strict Project Literal Scope (Object Type 30) if required
+        EXEC [SSISDB].[catalog].[set_execution_parameter_value] 
+            @execution_id, 
+            @object_type = 30, 
+            @parameter_name = N''Project::Source_File_Directory'', 
+            @parameter_value = N''c:\sc_CharmaneMchunu_2026\SSIS-Project-TG\Timesheets\'';
+    END CATCH;
+    
+    -- Fire off the package synchronously under the engine
+    EXEC [SSISDB].[catalog].[start_execution] @execution_id;
+';
 
-EXEC msdb.dbo.sp_add_jobstep 
+EXEC msdb.dbo.sp_add_jobstep     
     @job_id = @jobId, 
     @step_name = N'Run Integrated Package Process',
-    @subsystem = N'SSIS',
-    @command = @ssisCommand,
+    @subsystem = N'TSQL',              -- ◄ Changed from 'SSIS' to run under Engine file rights
+    @database_name = N'master',
+    @command = @tsqlCommand,
     @retry_attempts = 0;
 
 -- 4. Bind the High-Frequency 30-Second Schedule Matrix
-EXEC msdb.dbo.sp_add_jobschedule 
+EXEC msdb.dbo.sp_add_jobschedule     
     @job_id = @jobId, 
     @name = N'Timesheet_30Sec_Interval',
     @enabled = 1,
@@ -43,8 +74,7 @@ GO
 -- 5. Attach to Target Server Node Context
 DECLARE @jobId BINARY(16);
 SELECT @jobId = job_id FROM msdb.dbo.sysjobs WHERE name = N'Run_Timesheet_Package';
-
-EXEC msdb.dbo.sp_add_jobserver 
+EXEC msdb.dbo.sp_add_jobserver     
     @job_id = @jobId, 
     @server_name = @@SERVERNAME;
 GO
