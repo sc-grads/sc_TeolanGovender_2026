997ece49e2114c26034971162f92aa459941edf0
Teolan432 <teolan.govender@sambeconsulting.com>
2026-09-02 09:47:59 +0200

    Python refresh - 34

--- Flask API Python Course/script.py
+++ Flask API Python Course/script.py
@@ -1,8 +1,56 @@
-day_of_week = input("What day of the week is it today? ")
+student = {"name": "Rolf", "grades": (89, 90, 93, 78, 90)}
 
-if day_of_week == "Monday":
-    print("Have a great start to your week!")
-else:
-    print("Full speed ahead!")
 
-print("This always runs.")
+def average(sequence):
+    return sum(sequence) / len(sequence)
+
+
+print(average(student["grades"]))
+
+# But wouldn't it be nice if we could...
+# print(student.average()) ?
+
+
+class Student:
+    def __init__(self):
+        self.name = "Rolf"
+        self.grades = (89, 90, 93, 78, 90)
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student = Student()
+print(student.average())
+# Identical to Student.average(student)
+
+
+# -- Parameters in __init__ --
+
+
+class Student:
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student = Student("Bob", (36, 67, 90, 100, 100))
+print(student.average())
+
+# -- Remember *args ? --
+
+
+class Student:
+    def __init__(self, name, *grades):
+        self.name = name
+        self.grades = grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student = Student("Bob", 36, 67, 90, 100, 100)
+print(student.average())
