c63a3b0e7aa2eebe59e055383def232d8516fab5
Teolan432 <teolan.govender@sambeconsulting.com>
2026-09-04 11:57:33 +0200

    Python flask - sqlalchemy - 78

--- Flask API Python Course/app.py
+++ Flask API Python Course/app.py
@@ -1,89 +1,35 @@
-import uuid
-from flask import Flask, request
-from flask_smorest import abort
-from db import stores, items
+#app.py
+from flask import Flask
+from flask_smorest import Api
 
-app = Flask(__name__)
+from db import db
 
+import models
 
-#---------------------------------------Items---------------------------------------
-
-#--gets specific item--
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Store not found")
-
-#--creates an item--
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    # Here not only we need to validate data exists,
-    # But also what type of data. Price should be a float,
-    # for example...
-    if (
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ):
-        abort(
-            400,
-            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-        )
-    for item in items.values():
-        if (
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(400, message=f"Item already exists.")
-
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-
-    return item
-
-#--gets all items--
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
 
+def create_app(db_url=None):
+    app = Flask(__name__)
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+    api = Api(app)
 
-#---------------------------------------Stores---------------------------------------
+    with app.app_context():
+        db.create_all()
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        # Here you might also want to add the items in this store
-        # We'll do that later on in the course
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found")
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
 
-
-@app.post("/store") # Register route for HTTP POST requests to /store
-def create_store():
-    store_data = request.get_json() # Extract incoming JSON body into a dictionary
-    
-    if "name" not in store_data:
-        abort(
-            400,
-            message="Bad request. Ensure 'name' is included in the JSON payload.",
-        )
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message=f"Store already exists.")
-
-    store_id = uuid.uuid4().hex # Generate a unique ID string
-    store = {**store_data, "id": store_id} # Merge request data with the new ID
-    stores[store_id] = store # Store the new record in the main stores dictionary
-
-    return store # Return response (Flask automatically serializes to JSON)
-
-
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+    return app
