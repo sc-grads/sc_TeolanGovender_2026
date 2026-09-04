c0bd5e6791442b3cef214653efb10dbc36e12951
Teolan432 <teolan.govender@sambeconsulting.com>
2026-09-04 09:25:56 +0200

    Python refresh - dockersmorest

--- Flask API Python Course/app.py
+++ Flask API Python Course/app.py
@@ -1,48 +1,89 @@
+import uuid
 from flask import Flask, request
+from flask_smorest import abort
+from db import stores, items
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
 
-]
+#---------------------------------------Items---------------------------------------
 
-@app.get("/store") #http://127.0.0.1:5000/store
-def get_stores():
-        return {"stores": stores}
+#--gets specific item--
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Store not found")
 
-@app.post("/store")
+#--creates an item--
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example...
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+
+#--gets all items--
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+
+#---------------------------------------Stores---------------------------------------
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        # Here you might also want to add the items in this store
+        # We'll do that later on in the course
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+@app.post("/store") # Register route for HTTP POST requests to /store
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    print(stores)
-    return new_store, 201
+    store_data = request.get_json() # Extract incoming JSON body into a dictionary
+    
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "Store with that name does not exist"}, 404
+    store_id = uuid.uuid4().hex # Generate a unique ID string
+    store = {**store_data, "id": store_id} # Merge request data with the new ID
+    stores[store_id] = store # Store the new record in the main stores dictionary
 
-
-@app.get("/store 2") #http://127.0.0.1:5000/store
-def get_store2():
-
-        return {"stores": stores}
+    return store # Return response (Flask automatically serializes to JSON)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
